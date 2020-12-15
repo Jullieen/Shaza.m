65c72a3d0a9a799ee735b4d47155066614c759c3
Jullieen <74650245+Jullieen@users.noreply.github.com>
2020-12-15 17:51:13 +0100

    Update RunBPM.m

--- RunBPM.m
+++ RunBPM.m
@@ -1,34 +1,5 @@
 function [BPM]=RunBPM(AudioFile)
 
-
-%   Copyright (c) 2012 Eder de Souza.
-%
-%   This program is free software; you can redistribute it and/or modify        
-%   it under the terms of the GNU General Public License as published by        
-%   the Free Software Foundation; either version 2 of the License, or           
-%   (at your option) any later version.                                         
-%                                                                                
-%   This program is distributed in the hope that it will be useful,             
-%   but WITHOUT ANY WARRANTY; without even the implied warranty of              
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               
-%   GNU General Public License for more details.                                
-%                                                                                
-%   You should have received a copy of the GNU General Public License           
-%   along with this program; if not, write to the Free Software                 
-%   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  
-% 
-% ---------------------------------------------------------------------------------------------------------------------
-%
-%   The approach used in this code are described by George Tzanetakis,
-%   The beat calculation is made breaking the signal in sub-band frequencies using DWT(Discrete wavelet transform),
-%   After get all sub-bands we need envelope each band,
-%   sum all Envelopes to a single onset strength signal and apply auto-correlation to find the peak position. 
-%   
-%
-%How use:
-%
-%BPM=RunBPM('AudioTest/B128.wav')
-
 [Signal,Fs]=audioread(AudioFile);
 
 
